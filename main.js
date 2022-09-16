fe936c02c980def41c926202acfebdd0151a5317
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-15 21:27:20 -0500

    For...in

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ console.log('for of con una lista de numeros del 1 al 8')
 
 let iterable = [1, 2, 3, 4, 5, 6 ,7 ,8 ];
 
-for (let value of iterable) {
+for (let value in iterable) {
   console.log(value);
 }
 console.log('for of con una cadena de texto en este caso hola')
 let iterable1 = "hola";
 
-for (let value of iterable1) {
+for (let value in iterable1) {
   console.log(value);
 }
