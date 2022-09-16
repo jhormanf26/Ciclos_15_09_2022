800b3c6a384fb817fc84810a41b5104c465c6a00
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-15 21:21:57 -0500

    For...OF

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
+console.log('for of con una lista de numeros del 1 al 8')
 
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
+let iterable = [1, 2, 3, 4, 5, 6 ,7 ,8 ];
+
+for (let value of iterable) {
+  console.log(value);
 }
+console.log('for of con una cadena de texto en este caso hola')
+let iterable1 = "hola";
 
+for (let value of iterable1) {
+  console.log(value);
+}
